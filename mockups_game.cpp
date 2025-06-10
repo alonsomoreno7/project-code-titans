0a9d075b83d4950278f4105514c8ad4468795503
sofiamenjivar96 <00074525@uca.edu.sv>
2025-06-10 17:29:46 -0600

    Avance de mockups del proyecto

---
+++ mockups_game.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+                                 //Menus en terminal
+
+    // MENU BLACKJACK
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                    -PLAY BLACKJACK-                      . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*    Blackjack is a card game where the objective is to    . " << endl;
+    cout << "|    reach 21 without going over. You win if you get       . " << endl;
+    cout << ".    closer to 21 than the dealer.                         | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ".    MENU (M)                                 PLAY (P)     * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << " " << endl;
+    cout << " " << endl;
+
+    // MENU BLACKJACK elegir dificultad
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                       -BLACKJACK-                        . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                  Choose the difficulty:                  . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << ".   (1) EASY:      -Play only against the dealer.-         | " << endl;
+    cout << ".   (2) MEDIUM:    -The dealer draws cards up to 17,       *" << endl;
+    cout << ".                  “doubling down” is allowed.-            |" << endl;
+    cout << ".   (3) DIFFICULT: -You add split and side bets,           * " << endl;
+    cout << ".                  The dealer plays more aggressively.-    | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".     BACK (B)                                             |" << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *" << endl;
+    cout << endl;
+
+    // MENU ROULETTE
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                     -PLAY ROULETTE-                      .  " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*    Roulette is a game of chance in which the player      . " << endl;
+    cout << "|    bets where a ball will land on a numbered spinning    . " << endl;
+    cout << ".    wheel.                                                | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ".    MENU (M)                                 PLAY (P)     * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << " " << endl;
+    cout << " " << endl;
+
+    // MENU RULETA elegir dificultad
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                       -ROULETTE-                         .  " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                  Choose the difficulty:                  . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << ".   (1) EASY:      You only bet on color (red or black).   | " << endl;
+    cout << ".   (2) MEDIUM:    Exact number bets (from 0 to 36).       * " << endl;
+    cout << ".   (3) DIFFICULT: Bet: even/odd + color + number.         | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".     BACK (B)                                             | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << endl;
+
+    // MENU TRAGAMONEDAS
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                   -PLAY SLOT MACHINE-                    . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*    A slot machine is a game where a series of random     . " << endl;
+    cout << "|    symbols are displayed on each spin. If all the        . " << endl;
+    cout << ".    symbols on a line match, you win.                     | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ".    MENU (M)                                 PLAY (P)     * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << " " << endl;
+    cout << " " << endl;
+
+    // MENU TRAGAMONEDAS elegir dificultad
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                      -SLOT MACHINE-                      . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                  Choose the difficulty:                  . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << ".   (1) EASY:      A single line of symbols                | " << endl;
+    cout << ".   (2) MEDIUM:    3 paylines                              * " << endl;
+    cout << ".   (3) DIFFICULT: 5 paylines + special symbols            | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".     BACK (B)                                             | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << endl;
+
+    // MENU DADOS
+
+    cout << endl;
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                       -PLAY CRAPS-                       . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*    Craps is a game in which you bet on the outcome of    . " << endl;
+    cout << "|    two rolls of dice. If you rolls a 7 or 11, you win.   . " << endl;
+    cout << ".    If you roll a 2, 3, or 12, you lose.                  |  " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ".    MENU (M)                                 PLAY (P)     * " << endl;
+    cout << ".                                                          | " << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - * " << endl;
+    cout << " " << endl;
+    cout << " " << endl;
+
+    // MENU DADOS elegir dificultad
+
+    cout << "* - * - * - * - * - * - * - * - *. . . . . . . . . . . . . . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                         -CRAPS-                          .  " << endl;
+    cout << "|                                                          . " << endl;
+    cout << "*                  Choose the difficulty:                  . " << endl;
+    cout << "|                                                          . " << endl;
+    cout << ".   (1) EASY:    -Simple dice roll: 7 or 11 win,           | " << endl;
+    cout << ".                 2, 3, 12 lose.-                          *" << endl;
+    cout << ".   (2) MEDIUM:  -“point” is established must be repeated  |" << endl;
+    cout << ".                 until a victory or defeat is achieved.-  * " << endl;
+    cout << ".   (3) DIFFICULT: -More bets: (doubles, combinations).-   | " << endl;
+    cout << ".                                                          * " << endl;
+    cout << ".     BACK (B)                                             |" << endl;
+    cout << ". . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *" << endl;
+    cout << endl;
+
+    /*
+                           EXPLICAR JUEGO MENUS
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                    -PLAY BLACKJACK-                      .
+        |                                                          .
+        *    Blackjack is a card game where the objective is to    .
+        |    reach 21 without going over. You win if you get       .
+        .    closer to 21 than the dealer.                         |
+        .                                                          *
+        .                                                          |
+        .    MENU (M)                                 PLAY (P)     *
+        .                                                          |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                     -PLAY ROULETTE-                      .
+        |                                                          .
+        *    Roulette is a game of chance in which the player      .
+        |    bets where a ball will land on a numbered spinning    .
+        .    wheel.                                                |
+        .                                                          *
+        .                                                          |
+        .    MENU (M)                                 PLAY (P)     *
+        .                                                          |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                   -PLAY SLOT MACHINE-                    .
+        |                                                          .
+        *    A slot machine is a game where a series of random     .
+        |    symbols are displayed on each spin. If all the        .
+        .    symbols on a line match, you win.                     |
+        .                                                          *
+        .                                                          |
+        .    MENU (M)                                 PLAY (P)     *
+        .                                                          |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                       -PLAY CRAPS-                       .
+        |                                                          .
+        *    Craps is a game in which you bet on the outcome of    .
+        |    two rolls of dice. If you rolls a 7 or 11, you win.   .
+        .    If they roll a 2, 3, or 12, they lose.                |
+        .                                                          *
+        .                                                          |
+        .    MENU (M)                                 PLAY (P)     *
+        .                                                          |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+
+                         ELEJIR DIFICULTAD MENUS
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                      -SLOT MACHINE-                      .
+        |                                                          .
+        *                  Choose the difficulty:                  .
+        |                                                          .
+        .   (1) EASY:   A single line of symbols                   |
+        .   (2) MEDIUM:  3 paylines                                *
+        .   (3) DIFFICULT: 5 paylines + special symbols            |
+        .                                                          *
+        .     BACK (B)                                             |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                      -BLACKJACK-                         .
+        |                                                          .
+        *                  Choose the difficulty:                  .
+        |                                                          .
+        .   (1) EASY:      -Play only against the dealer.-         |
+        .   (2) MEDIUM:    -The dealer draws cards up to 17,       *
+        .                  “doubling down” is allowed.-            |
+        .   (3) DIFFICULT: -You add split and side bets,           *
+        .                  The dealer plays more aggressively.-    |
+        .                                                          *
+        .     BACK (B)                                             |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                       -ROULETTE-                         .
+        |                                                          .
+        *                  Choose the difficulty:                  .
+        |                                                          .
+        .   (1) EASY:      You only bet on color (red or black).   |
+        .   (2) MEDIUM:  Exact number bets (from 0 to 36).         *
+        .   (3) DIFFICULT: Bet: even/odd + color + number.         |
+        .                                                          *
+        .     BACK (B)                                             |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+        * - * - * - * - * - * - * - * - *. . . . . . . . . . . . . .
+        |                                                          .
+        *                         -CRAPS-                          .
+        |                                                          .
+        *                  Choose the difficulty:                  .
+        |                                                          .
+        .   (1) EASY:    -Simple dice roll: 7 or 11 win,           |
+        .                 2, 3, 12 lose.-                          *
+        .   (2) MEDIUM:  -“point” is established must be repeated  |
+        .                 until a victory or defeat is achieved.-  *
+        .   (3) DIFFICULT: -More bets: (doubles, combinations).-   |
+        .                                                          *
+        .     BACK (B)                                             |
+        . . . . . . . . . . . . . .* - * - * - * - * - * - * - * - *
+
+
+    */
+
+    return 0;
+}
