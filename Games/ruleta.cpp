08df08421a4a274abaa7ff79d3d89ed1613f2577
Camila Alessandra Menjivar Martinez <00095825@uca.edu.sv>
2025-06-19 19:06:19 -0600

    correcion

--- Games/ruleta.cpp
+++ Games/ruleta.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <limits>
+#include <algorithm>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -38,6 +39,39 @@ const char colores[NUMEROS]={
 const string rojo = "\033[31m";
 const string negro = "\033[30m";
 const string verde = "\033[32m";
-const string fondo blanco = "\033[47m";
+const string fondo_blanco = "\033[47m";
 const string reset = "\033[0m";
+
+//Funciones para limpiar consola y pausar
+#ifdef _WIN32
+void esperarMs (int ms) { Sleep (ms);}
+void limpiarConsola(){ system ("cls");}
+#else
+void esperarMs(int ms){ usleep(ms * 1000);}
+void limpiarConsola(){ cout << "\033[2J\033[H"; }
+#endif
+
+// esructura que representa la posicion (X y Y) de cada numero 
+struct Posicion{int x, y; };
+
+//Definir dimensiones y centro de la canvas para dibujar la ruleta
+
+const int WIDTH = 90;
+const int HEIGHT = 30;
+const int RADIO = 13;
+const int CENTRO_X = WIDTH / 2;
+const int CENTRO_Y = HEIGHT / 2;
+
+//arreglo para guardar posiciones calculadas
+Posicion posiciones[NUMEROS];
+
+
+//Calcular las posiciones den circulo para dibujar la ruleta 
+void calcularPosiciones() {
+    for (int i = 0; i < NUMEROS; i++) {
+        double angulo = (2 * M_PI * i / NUMEROS) - M_PI / 2;
+        posiciones[i].x = CENTRO_X + int(RADIO * cos(angulo) * 1.9); 
+        posiciones[i].y = CENTRO_Y + int(RADIO * sin(angulo));
+    }
+}
 
