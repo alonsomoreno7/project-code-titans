4cb9fc3391238283e61c6cc30edf0d101269e082
sofiamenjivar96 <00074525@uca.edu.sv>
2025-06-21 22:47:07 -0600

    Tragamonedas modo dificl

--- Games/slotM.cpp
+++ Games/slotM.cpp
@@ -391,7 +391,7 @@ bool jackpotM(char slots[4]){
 
 void mediumMode(){
 
-    char symbols[3]; //arreglo para guardar los simbolos aletorios
+    char symbols[4]; //arreglo para guardar los simbolos aletorios
     
     int playerLose; //Validar si el jugador gano o perdio
     string anotherRound; //guardar respuesta del jugador
@@ -418,7 +418,6 @@ void mediumMode(){
                     resetColor();
                     //el bucle se repite hasta que el usuario ingrese "P" 
                 }
-
             }
         
             while(true){
@@ -436,7 +435,7 @@ void mediumMode(){
                 
                     if(jackpotM(symbols)){
                         setColor(32); //color verde
-                        cout<<"            * JACKPOT!! *   "<<endl;
+                        cout<<"               * JACKPOT!! *   "<<endl;
                         resetColor();
                         cout<<endl;
                         cout<<" Congratulations!! you have earned: $x"<<endl;
@@ -446,7 +445,7 @@ void mediumMode(){
                         break;
                     }else{
                         setColor(36); //color cian
-                        cout<<"            KEEP TRYING "<<endl;
+                        cout<<"               KEEP TRYING "<<endl;
                         resetColor();
                         losePhrases();
                         playerLose=1;
@@ -461,9 +460,7 @@ void mediumMode(){
                     //si no se envia mensaje de error 
                     //el bucle se repite hasta que el usuario ingrese "!" 
                 }
-        
-            }
-            
+            }     
         }
 
         if(playerLose==1){
@@ -496,7 +493,6 @@ void mediumMode(){
                 resetColor();
                 //si no se envia mensaje de error y el bucle se repite
             }
-
         }
 
         if(anotherRound=="N"||anotherRound=="n"){ //validar si el juegador eligio si o no
@@ -507,8 +503,32 @@ void mediumMode(){
 
 //                                           HARD MODE
 
+void emptySlotsH(){
+    //Mostrar slots vacios al jugador
+    cout<<endl;
+    cout<<"* - * - * - * - * - * - * . . . . . . . . . . . ."<<endl;
+    cout<<"|                                               ."<<endl;
+    cout<<"*                 Try your luck!                ."<<endl;
+    cout<<"|                                               ."<<endl;
+    cout<<"*    ---------------------------------------    ."<<endl;
+    cout<<".   |       |       |       |       |       |   ."<< endl;
+    cout<<".   |   -   |   -   |   -   |   -   |   -   |   *" <<endl;
+    cout<<".   |       |       |       |       |       |   |"<< endl;
+    cout<<".    ---------------------------------------    *"<<endl;
+    cout<<".    | Pull the lever (!)|                      |"<<endl;
+    cout<<". . . . . . . . . . . . * - * - * - * - * - * - *"<<endl;
+    cout<<endl;
+    //se le indica al jugador que debe de presionar "!" para generar los simbolos aleatorios
+}
+
+char randomSymbolH (){
+    //Funcion para generar simbolos aleatorios
+    char symbol [3]= {'$','7','@'}; //arreglo con 4 simbolos
+    int randomP = rand()%3; //variable para elegir un numero del 0 al 4
+    return symbol[randomP]; //la funcion retornara un simbolo random
+}
+
 void symbolColorH(char slots[5]){
-    //Funcion para asignar un color diferente a cada simbolo
     for(int i=0;i<5;i++){ //leer el arreglo de simbolos
         switch(slots[i]){ //leer posisicion del arreglo
             case '$': //asignar a '$' color azul
@@ -523,7 +543,7 @@ void symbolColorH(char slots[5]){
             resetColor(); 
             cout<<"   | "<< "  ";
             break;
-            case '0': //asignar a '0' color magenta
+            case '@': //asignar a '0' color magenta
             setColor(35);
             cout<< slots[i];
             resetColor(); 
@@ -540,7 +560,148 @@ void symbolColorH(char slots[5]){
     }
 }
 
+void finalSlotsH (char slots[5]){
+    //Funcion para Mostrar slots finales al jugador
 
+    //funcion para asignar un color diferente a cada simbolo
+    
+    cout<<endl;
+    cout<<"* - * - * - * - * - * - * . . . . . . . . . . . ."<<endl;
+    cout<<"|                                               ."<<endl;
+    cout<<"*                    RESULTS!                   ."<<endl;
+    cout<<"|                                               ."<<endl;
+    cout<<"*    ---------------------------------------    ."<<endl;
+    cout<<".   |       |       |       |       |       |   ."<< endl;
+    //En esta linea se mostraran los simbolos aleatorios
+    cout<<".   |   ";
+    symbolColorH(slots);
+    cout<<"." <<endl;
+    cout<<".   |       |       |       |       |       |   |"<< endl;
+    cout<<".    ---------------------------------------    *"<<endl;
+    cout<<".    | Pull the lever (!)|                      |"<<endl;
+    cout<<". . . . . . . . . . . . * - * - * - * - * - * - *"<<endl;
+    cout<<endl;
+
+}
+
+bool jackpotH(char slots[5]){
+    //funcion para validar si los 3 simbolos son iguales
+    return(slots[0]==slots[1] && slots[1]==slots[2] && slots[2]==slots[3]&& slots[3]==slots[4]);
+    //si los tres son iguales = true
+}
+
+//Main game hard
+void hardMode(){
+
+    char symbols[3]; //arreglo para guardar los simbolos aletorios
+    
+    int playerLose; //Validar si el jugador gano o perdio
+    string anotherRound; //guardar respuesta del jugador
+
+    while (true){ // while infinito para que el jugador decida seguir jugando
+
+        srand(time(0)); //aleatoriedad
+        
+         for(int i=15;i>0;i--){ //bucle for que repite 15 veces cada tiro
+
+            while(true){ //while infinito para mostrar al jugador los tiros restantes 
+
+                string nextSpin; //variable para que el jugador inicie cada juego
+                cout<<endl;
+                cout<<"You have "<< i <<" remaining chances!"<<endl; //se le muestran al jugador las oportunidades restantes
+                cout<<"(P) to continue..."<<endl; //se le indica al jugador debe de presionar "P" para continuar.
+                getline(cin,nextSpin);  //se ingresa y se lee el character ingresado
+                if(nextSpin=="P"|| nextSpin=="p"){
+                    break; //si se ingresa "P" o "p" el bucle while termina y se continua con el juego.
+                }else{
+                    cout<<endl;
+                    setColor(31); //color rojo
+                    cout<<"Please enter a valid option (P), try again."<<endl; //si no se envia mensaje de error 
+                    resetColor();
+                    //el bucle se repite hasta que el usuario ingrese "P" 
+                }
+            }
+        
+            while(true){
+
+                string pull;
+                emptySlotsH(); //Se llama a la funcion Mostrar slots vacios al jugador 
+                getline(cin,pull); //el jugador debe ingresar "!" y se lee el character ingresado
+    
+                if(pull=="!"){
+                    //si se ingresa "!" se continua con el juego.
+                    for(int i=0;i<=5;i++){            //
+                        symbols[i]= randomSymbolH();
+                    }
+                    finalSlotsH(symbols); //Se llama a la funcion Mostrar slots finales al jugador
+                
+                    if(jackpotH(symbols)){
+                        setColor(32); //color verde
+                        cout<<"                   * JACKPOT!! *   "<<endl;
+                        resetColor();
+                        cout<<endl;
+                        cout<<" Congratulations!! you have earned: $x"<<endl;
+                        cout<<endl;
+                        i=0;
+                        playerLose=0;
+                        break;
+                    }else{
+                        setColor(36); //color cian
+                        cout<<"                   KEEP TRYING "<<endl;
+                        resetColor();
+                        losePhrases();
+                        playerLose=1;
+                        break;
+                    }
+                
+                }else{
+                    cout<<endl;
+                    setColor(31); //color rojo
+                    cout<<"Please enter a valid option (!), try again."<<endl;
+                    resetColor();
+                    //si no se envia mensaje de error 
+                    //el bucle se repite hasta que el usuario ingrese "!" 
+                }
+            }         
+        }
+
+        if(playerLose==1){
+            cout<<endl;
+            cout<<"   You'll be lucky next time."<<endl;
+            cout<<"-$100"<<endl;
+            cout<<endl;
+        }
+
+        while(true){ //while infinito para validar que se ingreso un caracter valido
+            cout<<"\nDo you want to play another round?"<<endl;
+            cout<<"     YES (Y).        NO(N)"<<endl;
+            getline(cin,anotherRound); //jugador ingresa su opccion y se lee su respuesta
+
+            if(anotherRound=="Y"||anotherRound=="y"){
+                cout<<endl;
+                setColor(32); //color verde
+                cout<<"GOOD LUCK!!"<<endl; //si se ingresa "Y" o "y" (yes) el bucle termina
+                resetColor();
+                anotherRound="";
+                break;
+            }else if(anotherRound=="N"||anotherRound=="n"){
+                cout<<endl;
+                cout<<"Return to menu..."<<endl; //si se ingresa "N" o "n" (no) el bucle termina
+                break;
+            }else{
+                cout<<endl;
+                setColor(31); //color rojo
+                cout<<"Please enter a valid option (Y) or (N), try again."<<endl; 
+                resetColor();
+                //si no se envia mensaje de error y el bucle se repite
+            }
+        }
+
+        if(anotherRound=="N"||anotherRound=="n"){ //validar si el juegador eligio si o no
+            break; //si el usuario ingreso "N" o "n" (no) la funcion termina
+        }   
+    }
+}
 
 
 //juego principal
@@ -572,14 +733,11 @@ int main(){
                     easyMode(); //llama a funcion easyMode (Juego modo facil)
                 } else if (option == "M"||option=="m"){
                     mediumMode(); //llama a funcion mediumMode (juego modo medio)
-
                 } else if (option == "H"||option=="h"){
-
+                    hardMode(); //llama a funcion hardMode (juego modo dificil)
                 } else {
-
                     // se muestra mensaje de error y se vuelve a pedir ingresar una opccion
-                    //color rojo
-                    setColor(31);
+                    setColor(31); //color rojo
                     cout << "Please enter a valid option (E),(M),(D) or (B), try again." << endl;
                     resetColor();
                 }
